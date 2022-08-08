9512d2ebf3810cf25ba4c5e281e8c148050a0c7f
Eduardo <eduhmq@gmail.com>
2022-08-08 00:02:25 -0300

    javascript services finished

---
+++ services/index.js
@@ -0,0 +1,20 @@
+
+
+function showData() {
+    var finalDate = new Date("Nov 15, 2022 18:22:40").getTime();
+    var nowTime = new Date().getTime();
+    var difference = finalDate - nowTime;
+
+    var days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+}
+
+setInterval(showData, 1000);
